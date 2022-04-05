52fbcd810dec4fce88d847b070258b363609ea29
Brady Bellini <bbellini@pm.me>
2022-04-04 18:01:38 -0700

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 api = Api(app)
 
 app.config.from_object('src.config.DevelopmentConfig')
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
 
 class Ping(Resource):
     def get(self):
